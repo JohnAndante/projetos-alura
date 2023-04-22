c5b53ba4393bbece730935f856eeef231a964473
Walker Silvestre <53200345+JohnAndante@users.noreply.github.com>
2023-04-22 10:41:27 -0300

    Update maior_ou_menor.rb

--- 02 - Estudos - Ruby on Rails/01 - Ruby/maior_ou_menor.rb
+++ 02 - Estudos - Ruby on Rails/01 - Ruby/maior_ou_menor.rb
@@ -7,14 +7,26 @@ puts "\nComeçaremos o nome para você, " + nome + "\n"
 puts "Escolhendo um número secreto entre 0 e 200.."
 numero_secreto = 170
 puts "\nNúmero escolhido, que tal adivinhar nosso número secreto??"
+qtd_tentativas = 5
 
-numero = gets
-puts "\nSua tentativa é " + numero
-puts "\nSerá que está correto?"
+for tentativa in 1..qtd_tentativas
+  puts "\n\n\n\n"
+  puts "Tentativa " + tentativa.to_s + " de " + qtd_tentativas.to_s
+  puts "Entre com o número"
+  chute = gets
+  puts "Você chutou " + chute.to_s
 
-if numero_secreto == numero.to_i
-  puts "Acertou!!"
-else
-  puts "Errou!!"
+  acertou = numero_secreto == chute.to_i
+
+  if acertou
+    puts "Acertou!!!"
+    exit 0
+  else
+    maior = numero_secreto > chute.to_i
+    if maior
+      puts "O número secreto é maior!!!"
+    else
+      puts "O número secreto é menor!!"
+    end
+  end
 end
-
