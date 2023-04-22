a757380e0efba2b8818dc85378e72c6ed36ceb72
Walker Silvestre <53200345+JohnAndante@users.noreply.github.com>
2023-04-22 11:08:27 -0300

    Update maior_ou_menor.rb

--- 02 - Estudos - Ruby on Rails/01 - Ruby/maior_ou_menor.rb
+++ 02 - Estudos - Ruby on Rails/01 - Ruby/maior_ou_menor.rb
@@ -9,9 +9,39 @@ end
 # Sorteia numero secreto
 def sorteia_numero_secreto
   puts "Escolhendo um número secreto entre 0 e 200.."
-  numero_secreto = 170
+  sorteado = 170
   puts "\nNúmero escolhido, que tal adivinhar nosso número secreto??"
-  numero_secreto
+  sorteado
+end
+
+# Pede um numero para o usuario
+def pede_um_numero(tentativa, qtd_tentativas)
+  puts "\n\n\n\n"
+  puts "Tentativa " + tentativa.to_s + " de " + qtd_tentativas.to_s
+  puts "Entre com o número"
+  chute = gets
+  puts "Você chutou " + chute.to_s
+  chute.to_i
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+
+  acertou = numero_secreto == chute
+
+  if acertou
+    puts "Acertou!!!"
+    return true
+  end
+
+
+  maior = numero_secreto > chute
+  if maior
+    puts "O número secreto é maior!!!"
+  else
+    puts "O número secreto é menor!!"
+  end
+  false
+
 end
 
 #######################################################################
@@ -24,23 +54,6 @@ qtd_tentativas = 5
 
 # Início do for de adivinhação
 for tentativa in 1..qtd_tentativas
-  puts "\n\n\n\n"
-  puts "Tentativa " + tentativa.to_s + " de " + qtd_tentativas.to_s
-  puts "Entre com o número"
-  chute = gets
-  puts "Você chutou " + chute.to_s
-
-  acertou = numero_secreto == chute.to_i
-
-  if acertou
-    puts "Acertou!!!"
-    break
-  else
-    maior = numero_secreto > chute.to_i
-    if maior
-      puts "O número secreto é maior!!!"
-    else
-      puts "O número secreto é menor!!"
-    end
-  end
+  chute = pede_um_numero tentativa, qtd_tentativas
+  break if verifica_se_acertou numero_secreto, chute
 end
