7e7989f20f0a2069a9c7badae6e82a3a737b4678
Walker Silvestre <walker_contas@hotmail.com>
2023-05-09 16:29:34 -0300

    Aula: Colisão

--- 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
+++ 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
@@ -38,8 +38,16 @@ def joga(nome)
     desenha mapa
     direcao = pede_movimento
     heroi = encontra_jogador mapa
-    mapa[heroi[0]][heroi[1]] = " "
     nova_posicao = calcula_nova_posicao heroi, direcao
+
+    if  (nova_posicao[0] < 0 || nova_posicao[1] < 0 || nova_posicao[0] >= mapa.size || nova_posicao[1] >= mapa[0].size)
+      next
+    end
+
+    if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
+      next
+    end
+    mapa[heroi[0]][heroi[1]] = " "
     mapa[nova_posicao[0]][nova_posicao[1]] = "H"
   end
 end
