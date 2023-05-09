5866362623aa2e41369dd1bbe6bb173170d5627a
Walker Silvestre <walker_contas@hotmail.com>
2023-05-09 16:42:14 -0300

    Aula: Validação

--- 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
+++ 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
@@ -32,6 +32,22 @@ def calcula_nova_posicao(heroi, posicao)
   novo_heroi
 end
 
+def posicao_valida?(mapa, posicao)
+  linhas = mapa.size
+  colunas = mapa[0].size
+
+  estourou_linhas = posicao[0] < 0 || posicao[0] >= linhas
+  estourou_colunas = posicao[1] < 0 || posicao[1] >= colunas
+
+  bateu_na_parede = mapa[posicao[0]][posicao[1]] == "X"
+
+  if  estourou_linhas || estourou_colunas || bateu_na_parede
+    return false
+  end
+
+  true
+end
+
 def joga(nome)
   mapa = le_mapa 1
 
@@ -41,13 +57,10 @@ def joga(nome)
     heroi = encontra_jogador mapa
     nova_posicao = calcula_nova_posicao heroi, direcao
 
-    if  (nova_posicao[0] < 0 || nova_posicao[1] < 0 || nova_posicao[0] >= mapa.size || nova_posicao[1] >= mapa[0].size)
+    if !posicao_valida? mapa, nova_posicao
       next
     end
 
-    if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
-      next
-    end
     mapa[heroi[0]][heroi[1]] = " "
     mapa[nova_posicao[0]][nova_posicao[1]] = "H"
   end
