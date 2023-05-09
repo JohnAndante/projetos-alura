c14c823fc6c314983c44cb9cc0b932179739dcf9
Walker Silvestre <walker_contas@hotmail.com>
2023-05-09 16:08:25 -0300

    Aula: Retorno

--- 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
+++ 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
@@ -10,8 +10,8 @@ def encontra_jogador mapa
   caractere_do_heroi = "H"
   mapa.each_with_index do |linha_atual, linha|
     coluna_do_heroi = linha_atual.index caractere_do_heroi
-    if coluna_do_heroic
-      # achei linha e coluna!
+    if coluna_do_heroi
+      return [linha, coluna_do_heroi]
     end
   end
   # não achei
@@ -23,6 +23,20 @@ def joga(nome)
   while true
     desenha mapa
     direcao = pede_movimento
+    heroi = encontra_jogador mapa
+    mapa[heroi[0]][heroi[1]] = " "
+
+    case direcao
+      when "W"
+        heroi[0] -= 1
+      when "S"
+        heroi[0] += 1
+      when "A"
+        heroi[1] -= 1
+      when "D"
+        heroi[1] -= 1
+    end
+    mapa[heroi[0]][heroi[1]] = "H"
   end
 end
 
