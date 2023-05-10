b95230dbce20b5485600074d915e2f733751a82a
JohnAndante <walker_contas@hotmail.com>
2023-05-09 22:01:50 -0300

    Aula: Aleatório

--- 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
+++ 02 - Estudos - Ruby on Rails/01 - Ruby/fogefoge/fogefoge.rb
@@ -50,55 +50,49 @@ def posicao_valida?(mapa, posicao)
   true
 end
 
-def posicoes_validas_a_partir_de(mapa, posicao)
+def soma_vetor(vetor1, vetor2)
+  [vetor1[0] + vetor2[0], vetor1[1] + vetor2[1]]
+end
+
+def posicoes_validas_a_partir_de(mapa, novo_mapa, posicao)
   posicoes = []
-  baixo     = [posicao[0] + 1, posicao[1]]
-  if posicao_valida? mapa, baixo
-    posicoes << baixo
+  movimentos = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+  movimentos.each do |movimento|
+    nova_posicao = soma_vetor(movimento, posicao)
+    if posicao_valida?(mapa, nova_posicao) && posicao_valida?(novo_mapa, nova_posicao)
+      posicoes << nova_posicao
+    end
   end
-
-  direita   = [posicao[0], posicao[1] + 1]
-  if posicao_valida? mapa, direita
-    posicoes << direita
-  end
-
-  cima      = [posicao[0] - 1, posicao[1]]
-  if posicao_valida? mapa, cima
-    posicoes << cima
-  end
-
-  esquerda  = [posicao[0], posicao[1] - 1]
-  if posicao_valida? mapa, esquerda
-    posicoes << esquerda
-  end
-
   posicoes
 end
 
-def move_fantasma(mapa, linha, coluna)
-  posicoes = posicoes_validas_a_partir_de mapa, [linha, coluna]
+def move_fantasma(mapa, novo_mapa, linha, coluna)
+  posicoes = posicoes_validas_a_partir_de mapa, novo_mapa, [linha, coluna]
   return if posicoes.empty?
-  posicao = posicoes[0]
 
+  aleatoria = rand posicoes.size
+  posicao = posicoes[aleatoria]
   mapa[linha][coluna] = " "
-  mapa[posicao[0]][posicao[1]] = "F"
+  novo_mapa[posicao[0]][posicao[1]] = "F"
 end
 
 def copia_mapa(mapa)
-  mapa.join("\n").tr("F", " ").split "\n"
+  novo_mapa = mapa.join("\n").tr("F", " ").split "\n"
   novo_mapa
 end
 
 def move_fantasmas(mapa)
   caractere_do_fantasma = "F"
+  novo_mapa = copia_mapa mapa
   mapa.each_with_index do |linha_atual, linha|
     linha_atual.chars.each_with_index do |caractere_atual, coluna|
       eh_fantasma = caractere_atual == caractere_do_fantasma
       if eh_fantasma
-        move_fantasma mapa, linha, coluna
+        move_fantasma mapa, novo_mapa, linha, coluna
       end
     end
   end
+  novo_mapa
 end
 
 def joga(nome)
@@ -117,7 +111,7 @@ def joga(nome)
     mapa[heroi[0]][heroi[1]] = " "
     mapa[nova_posicao[0]][nova_posicao[1]] = "H"
 
-    move_fantasmas mapa
+    mapa = move_fantasmas mapa
   end
 end
 
