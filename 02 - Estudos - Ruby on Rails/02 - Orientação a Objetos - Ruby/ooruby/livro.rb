6f5cbed2a88bde2faae015a81de0124f389b3d7d
Walker Silvestre <walker_contas@hotmail.com>
2023-05-18 17:31:31 -0300

    Modelos Anêmicos e Encapsulamento

--- 02 - Estudos - Ruby on Rails/02 - Orientação a Objetos - Ruby/ooruby/livro.rb
+++ 02 - Estudos - Ruby on Rails/02 - Orientação a Objetos - Ruby/ooruby/livro.rb
@@ -1,47 +1,84 @@
 class Livro
-  attr_reader :titulo, :preco, :ano_lancamento
+  attr_reader :titulo, :preco, :ano_lancamento, :possui_reimpressao
 
-  def initialize(titulo, preco, ano_lancamento)
+  def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
     @titulo = titulo
     @preco = preco
     @ano_lancamento = ano_lancamento
+    @possui_reimpressao = possui_reimpressao
+  end
+
+  def to_csv
+    "#{@titulo},#{@ano_lancamento},#{@preco}"
+  end
+
+  def possui_reimpressao?
+    @possui_reimpressao
   end
 
   private
+
   def calcula_preco(base)
-    if @ano_lancamento < 2000
-      base * 0.7
+    if @ano_lancamento < 2006
+      if @possui_reimpressao
+        base * 0.9
+      else
+        base * 0.05
+      end
+    elsif @ano_lancamento <= 2010
+      base * 0.96
     else
       base
     end
   end
 end
 
-livro_rails = Livro.new("Agile Web Development with Rails", 70, 2011)
-livro_ruby = Livro.new("Programming Ruby 1.9", 60, 2010)
-livro_algoritmos = Livro.new("Algoritmos", 100, 1998)
+class Estoque
+  def initialize
+    @livros = []
+  end
 
-def imprime_nota_fiscal(livros)
-  livros.each do |livro|
-    puts "Titulo: #{livro.titulo} - #{livro.preco}"
+  def exporta_csv
+    @livros.each do |livro|
+      puts livro.to_csv
+    end
+  end
+
+  def mais_baratos_que(valor)
+    @livros.select do |livro|
+      livro.preco <= valor
+    end
+  end
+
+  def total
+    @livros.size
+  end
+
+  def adiciona(livro)
+    @livros << livro if livro
   end
 end
 
-def livro_para_newsletter(livro)
-  if livro.ano_lancamento < 1999
-    puts "Newsletter/Liquidação"
-    puts "Titulo: #{livro.titulo} - #{livro.preco}"
-  end
+livro_algoritmos = Livro.new("Algoritmos", 100, 1998, true)
+livro_arquitetura = Livro.new("Introdução a Arquitetura e Design de Software", 70, 2011, true)
+
+estoque = Estoque.new
+estoque.adiciona livro_algoritmos
+estoque.adiciona livro_arquitetura
+estoque.adiciona Livro.new("The Pragmatic Programmer", 100, 1999, true)
+estoque.adiciona Livro.new("Programmin Ruby", 100, 2004, true)
+estoque.adiciona nil
+
+puts "\n\n"
+
+estoque.exporta_csv()
+
+puts "\n\n"
+
+baratos = estoque.mais_baratos_que(80)
+baratos.each do |livro|
+  puts livro.titulo
 end
-
-puts "\n\n"
-
-livros = [livro_rails,livro_ruby]
-imprime_nota_fiscal livros
-
-puts "\n\n"
-
-livro_para_newsletter(livro_algoritmos)
 
 puts "\n\n"
 
