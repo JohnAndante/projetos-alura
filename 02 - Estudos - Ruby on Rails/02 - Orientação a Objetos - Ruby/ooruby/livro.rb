b601fa218b610b747d99198fa3b51ef036e91286
JohnAndante <walker_contas@hotmail.com>
2023-05-30 22:33:29 -0300

    Enxugando seu código

--- 02 - Estudos - Ruby on Rails/02 - Orientação a Objetos - Ruby/ooruby/livro.rb
+++ 02 - Estudos - Ruby on Rails/02 - Orientação a Objetos - Ruby/ooruby/livro.rb
@@ -1,12 +1,14 @@
 class Livro
-  attr_reader :titulo, :preco, :ano_lancamento, :possui_reimpressao
+  attr_reader :titulo, :preco, :ano_lancamento, :editora, :tipo
 
-  def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
-    @titulo = titulo
-    @preco = preco
-    @ano_lancamento = ano_lancamento
-    @possui_reimpressao = possui_reimpressao
-  end
+    def initialize(titulo, preco, ano_lancamento, possui_reimpressao, editora, tipo)
+      @titulo = titulo
+      @ano_lancamento = ano_lancamento
+      @preco = calcula_preco(preco)
+      @possui_reimpressao = possui_reimpressao
+      @editora = editora
+      @tipo = tipo
+    end
 
   def to_csv
     "#{@titulo},#{@ano_lancamento},#{@preco}"
@@ -32,73 +34,3 @@ class Livro
     end
   end
 end
-
-module Contador
-  def <<(livro)
-    push(livro)
-    if @maximo_necessario.nil? || @maximo_necessario < size
-      @maximo_necessario = size
-    end
-    self
-  end
-
-  attr_reader :maximo_necessario
-end
-
-class Estoque
-  attr_reader :livros
-
-  def initialize
-    @livros = []
-    @livros.extend Contador
-  end
-
-  def exporta_csv
-    @livros.each do |livro|
-      puts livro.to_csv
-    end
-  end
-
-  def mais_baratos_que(valor)
-    @livros.select do |livro|
-      livro.preco <= valor
-    end
-  end
-
-  def total
-    @livros.size
-  end
-
-  def <<(livro)
-    @livros << livro if livro
-    self
-  end
-
-  def remove(livro)
-    @livros.delete livro
-  end
-
-  def maximo_necessario
-    @livros.maximo_necessario
-  end
-end
-
-livro_algoritmos = Livro.new("Algoritmos", 100, 1998, true)
-livro_arquitetura = Livro.new("Introdução a Arquitetura e Design de Software", 70, 2011, true)
-livro_programmer = Livro.new("The Pragmatic Programmer", 100, 1999, true)
-livro_ruby = Livro.new("Programmin Ruby", 100, 2004, true)
-
-estoque = Estoque.new
-estoque << livro_algoritmos
-
-puts estoque.maximo_necessario
-
-estoque << livro_arquitetura << livro_programmer
-estoque << livro_ruby
-
-puts estoque.maximo_necessario
-
-estoque.remove livro_algoritmos
-
-puts estoque.maximo_necessario
-
