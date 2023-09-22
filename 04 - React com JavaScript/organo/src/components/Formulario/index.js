196646a4938ff6d20ac85e5dfdef1c3822d6d3e7
JohnAndante <walker_contas@hotmail.com>
2023-09-21 22:09:53 -0300

    Update Curso
    Aula 02 - Trabalhando com props concluída

--- 04 - React com JavaScript/organo/src/components/Formulario/index.js
+++ 04 - React com JavaScript/organo/src/components/Formulario/index.js
@@ -1,14 +1,44 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto/";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Insira uma imagem" />
+        <CampoTexto
+          required={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          required={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+        />
+        <CampoTexto
+          required={true}
+          label="Imagem"
+          placeholder="Insira uma imagem"
+        />
+        <ListaSuspensa required={true} itens={times} label="Times" />
+        <Botao texto="Criar Card" />
       </form>
     </section>
   );
