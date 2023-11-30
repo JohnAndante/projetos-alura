eeb888b98749d337544367f39a8362025a4a412b
JohnAndante <walker_contas@hotmail.com>
2023-11-29 22:54:13 -0300

    Update

--- 04 - React com JavaScript/organo/src/components/Time/index.js
+++ 04 - React com JavaScript/organo/src/components/Time/index.js
@@ -2,8 +2,8 @@ import "./Time.css";
 
 const Time = (props) => {
   return (
-    <section className="time">
-      <h3>{props.nome}</h3>
+    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+      <h3 style={{ borderBottomColor: props.corPrimaria }}>{props.nome}</h3>
     </section>
   );
 };
