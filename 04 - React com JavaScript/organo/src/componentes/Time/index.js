a654fa70c09583cd616c5678dea4b7fb9b3db09a
JohnAndante <walker_contas@hotmail.com>
2023-12-06 22:25:14 -0300

    Update Fim do Curso
    Curso encerrado

--- 04 - React com JavaScript/organo/src/componentes/Time/index.js
+++ 04 - React com JavaScript/organo/src/componentes/Time/index.js
@@ -1,17 +1,29 @@
-import Colaborador from '../Colaborador'
-import './time.css'
+import Colaborador from '../Colaborador';
+import './time.css';
+import hexToRgba from 'hex-to-rgba';
 
-const Time = ({ time, colaboradores }) => {
-    return (
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
+  return (
+    colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, 0.3) }}>
+      <input type='color' value={time.cor} onChange={(e) => mudarCor(e.target.value, time.id)} className='input-cor' />
+      <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+      <div className='colaboradores'>
+        {colaboradores.map((colaborador, indice) => {
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
-            <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
-            </div>
-        </section>
+          return (
+            <Colaborador
+              key={indice}
+              colaborador={colaborador}
+              corDeFundo={time.cor}
+              aoDeletar={aoDeletar}
+              aoFavoritar={aoFavoritar}
+            />
+          )
+        })}
+      </div>
+    </section>
 
-    )
+  )
 }
 
 export default Time
