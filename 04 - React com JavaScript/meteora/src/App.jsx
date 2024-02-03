9f205a3629c05ea5efc0c72fbe6cae4c410d8a2a
JohnAndante <walker_contas@hotmail.com>
2024-02-02 22:23:47 -0300

    Fim Aula 01

--- 04 - React com JavaScript/meteora/src/App.jsx
+++ 04 - React com JavaScript/meteora/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
+
 import Home from "./pages/Home";
 import Carrinho from "./pages/Carrinho";
 import PaginaErro from "./pages/PaginaErro";
