0b7d8168e7ec3051cc19f8adb700b5873f193a66
JohnAndante <walker_contas@hotmail.com>
2025-01-20 23:23:15 -0300

    #10 - feat: End of class '02. Estrutura básica do Router DOM'

--- 10 - Gerencie Aplicações React com Typescript/aluroni-router/eslint.config.mjs
+++ 10 - Gerencie Aplicações React com Typescript/aluroni-router/eslint.config.mjs
@@ -1,11 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
@@ -13,7 +13,10 @@ export default [
         ...pluginReact.configs.flat.recommended,
         rules: {
             ...pluginReact.configs.flat.recommended.rules,
-            "react/react-in-jsx-scope": "off"
+            'react/react-in-jsx-scope': 'off',
+            quotes: ['error', 'single'],
+            'linebreak-style': ['error', 'unix'],
+            semi: ['error', 'always']
         }
     }
 ];
--- 10 - Gerencie Aplicações React com Typescript/aluroni-router/src/pages/Inicio/index.tsx
+++ 10 - Gerencie Aplicações React com Typescript/aluroni-router/src/pages/Inicio/index.tsx
@@ -1,10 +1,27 @@
-import cardapio from "pages/Cardapio/Itens/itens.json"
+import cardapio from 'data/cardapio.json';
+import styles from './Inicio.module.scss';
 
 export default function Inicio() {
+    let pratosRecomendados = [...cardapio];
+    pratosRecomendados = pratosRecomendados
+        .sort(() => 0.5 - Math.random()).splice(0, 3)
+        .splice(0, 3);
+
     return (
         <section>
-            <h3>Recomendações da cozinha</h3>
-            <div></div>
-        </section>
-    )
+            <h3 className={styles.titulo}>Recomendações da cozinha</h3>
+            <div className={styles.recomendados}>
+                {pratosRecomendados.map(prato => (
+                    <div key={prato.id} className={styles.recomendado}>
+                        <div className={styles.recomendado__imagem}>
+                            <img src={prato.photo} alt={prato.title} />
+                        </div>
+                        <button className={styles.recomendado__botao}>
+                            Ver Mais
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </section >
+    );
 }
