6925a4871f6a987b8c978a5302aa4f7fb901a778
JohnAndante <walker_contas@hotmail.com>
2025-01-28 22:46:05 -0300

    #10 - feat: Start of course 'React: testando os seus componentes'

---
+++ 10 - Gerencie Aplicações React com Typescript/sorteador-de-amigo-secreto/src/componentes/Formulario.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Formulario from "./Formulario";
+
+// Jest
+
+test('Quando o input está vazio, novos participantes não podem ser adicionados', () => {
+
+    render(<Formulario />);
+
+    // Encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+
+    // Encontrar o botão
+    const botao = screen.getByRole('button');
+
+    // Garantir que o input esteja no documento
+    expect(input).toBeInTheDocument();
+
+    // Garantir que o botão esteja no documento
+    expect(botao).toBeDisabled();
+});
---
+++ 10 - Gerencie Aplicações React com Typescript/sorteador-de-amigo-secreto/src/componentes/Formulario.tsx
@@ -0,0 +1,11 @@
+
+const Formulario = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Insira os nomes dos participantes" />
+            <button disabled={true}>Adicionar</button>
+        </div>
+    );
+}
+
+export default Formulario;
