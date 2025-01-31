0add95ad91dea5bb1d5503394f20cc12b09ef103
JohnAndante <walker_contas@hotmail.com>
2025-01-30 21:29:30 -0300

    #10 - feat: End of class '02. Testando comportamentos'

--- 10 - Gerencie Aplicações React com Typescript/sorteador-de-amigo-secreto/src/componentes/Formulario.tsx
+++ 10 - Gerencie Aplicações React com Typescript/sorteador-de-amigo-secreto/src/componentes/Formulario.tsx
@@ -1,10 +1,38 @@
+import { useRef, useState } from "react";
+import { useAdicionarParticipante } from "../state/hooks/useAdicionarParticipante";
+import { useMensagemDeErro } from "../state/hooks/useMensagemDeErro";
 
 const Formulario = () => {
+    const [nome, setNome] = useState('');
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const adicionarNaLista = useAdicionarParticipante();
+
+    const mensagemDeErro = useMensagemDeErro();
+
+    const adicionarParticipante = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        adicionarNaLista(nome);
+
+        setNome('');
+        inputRef.current?.focus();
+    }
+
     return (
-        <div>
-            <input type="text" placeholder="Insira os nomes dos participantes" />
-            <button disabled={true}>Adicionar</button>
-        </div>
+        <form onSubmit={adicionarParticipante}>
+            <div>
+                <input
+                    ref={inputRef}
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                    type="text"
+                    placeholder="Insira os nomes dos participantes"
+                />
+                <button disabled={!nome}>Adicionar</button>
+            </div>
+            {mensagemDeErro && <p role="alert">{mensagemDeErro}</p>}
+        </form>
     );
 }
 
