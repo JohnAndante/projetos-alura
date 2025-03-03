44b2b1bd35b3b86c7fda9cee597281ef193437b2
JohnAndante <walker_contas@hotmail.com>
2025-03-03 18:36:30 -0300

    #12 - feat: End of class '04. Testando uma API'

--- 12 - Autenticação testes e segurança com Nodejs/Testes unitários e de integração/livraria/src/routes/livrosRoutes.js
+++ 12 - Autenticação testes e segurança com Nodejs/Testes unitários e de integração/livraria/src/routes/livrosRoutes.js
@@ -4,10 +4,10 @@ import LivrosController from '../controllers/livrosController.js';
 const router = express.Router();
 
 router
-  .get('/livros', LivrosController.listarLivros)
-  .get('/livros/:id', LivrosController.listarLivroPorId)
-  .post('/livros', LivrosController.cadastrarLivro)
-  .put('/livros/:id', LivrosController.atualizarLivro)
-  .delete('/livros/:id', LivrosController.excluirLivro);
+    .get('/livros', LivrosController.listarLivros)
+    .get('/livros/:id', LivrosController.listarLivroPorId)
+    .post('/livros', LivrosController.cadastrarLivro)
+    .put('/livros/:id', LivrosController.atualizarLivro)
+    .delete('/livros/:id', LivrosController.excluirLivro);
 
 export default router;
---
+++ 12 - Autenticação testes e segurança com Nodejs/Testes unitários e de integração/livraria/src/test/models/editora.test.js
@@ -0,0 +1,60 @@
+import {
+    describe, expect, it, jest,
+} from '@jest/globals';
+import Editora from '../../models/editora';
+
+describe('Testando o modelo Editora', () => {
+    const objetoEditora = {
+        nome: 'CDC',
+        cidade: 'São Paulo',
+        email: 'cdc@emails.com',
+    };
+
+    it('Deve instanciar um objeto Editora', () => {
+        const editora = new Editora(objetoEditora);
+
+        expect(editora).toEqual(expect.objectContaining(objetoEditora));
+    });
+
+    it.skip('Deve salvar a editora no banco de dados', () => {
+        const editora = new Editora(objetoEditora);
+
+        editora.salvar().then((resultado) => {
+            expect(resultado.nome).toBe(objetoEditora.nome);
+        });
+    });
+
+    it.skip('Deve salvar no banco de dados utilizando async/await', async () => {
+        const editora = new Editora(objetoEditora);
+
+        const resultado = await editora.salvar();
+
+        const retornado = await Editora.pegarPeloId(resultado.id);
+
+        expect(retornado).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                ...objetoEditora,
+                created_at: expect.any(String),
+                updated_at: expect.any(String),
+            }),
+        );
+    });
+
+    it('Deve fazer uma chamada simulada ao banco de dados', async () => {
+        const editora = new Editora(objetoEditora);
+
+        const mockSalvar = jest.fn().mockReturnValue({
+            id: 999,
+            ...objetoEditora,
+            created_at: new Date(),
+            updated_at: new Date(),
+        });
+
+        editora.salvar = mockSalvar;
+
+        await editora.salvar();
+
+        expect(mockSalvar).toHaveBeenCalled();
+    });
+});
