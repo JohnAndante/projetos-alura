65ae878c666070b591aa7aea1f51bdcc9b7176c0
JohnAndante <walker_contas@hotmail.com>
2025-03-04 01:11:56 -0300

    #12 - feat: Added missing bearer auth token

--- 12 - Autenticação testes e segurança com Nodejs/Implementando testes em uma API Rest/curso-nodejs-teste/src/test/routes/editorasRoutes.test.js
+++ 12 - Autenticação testes e segurança com Nodejs/Implementando testes em uma API Rest/curso-nodejs-teste/src/test/routes/editorasRoutes.test.js
@@ -5,6 +5,8 @@ import {
 import app from '../../app.js';
 
 let server;
+const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJ0ZXN0LXVzZXJAZW1haWwuY29tIiwiaWF0IjoxNzQxMDYxNDIyLCJleHAiOjE3NDExNDc4MjJ9.HdCl3lb2Hg00nuJelhHgwemSxuMp1KMTMQuHSnb7dvA';
+
 beforeEach(() => {
     const port = 3000;
     server = app.listen(port);
@@ -19,6 +21,7 @@ describe('GET em /editoras', () => {
         const resposta = await request(app)
             .get('/editoras')
             .set('Accept', 'application/json')
+            .set('Authorization', `Bearer ${accessToken}`)
             .expect(200);
 
         expect(resposta.body[0].email).toEqual('e@e.com');
@@ -30,6 +33,7 @@ describe('POST em /editoras', () => {
     it('Deve adicionar uma nova editora', async () => {
         const resposta = await request(app)
             .post('/editoras')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({
                 nome: 'CDC',
                 cidade: 'Sao Paulo',
@@ -42,6 +46,7 @@ describe('POST em /editoras', () => {
     it('Deve nao adicionar nada ao passar o body vazio', async () => {
         await request(app)
             .post('/editoras')
+            .set('Authorization', `Bearer ${accessToken}`)
             .send({})
             .expect(400);
     });
@@ -51,6 +56,7 @@ describe('GET em /editoras/id', () => {
     it('Deve retornar recurso selecionado', async () => {
         await request(app)
             .get(`/editoras/${idResposta}`)
+            .set('Authorization', `Bearer ${accessToken}`)
             .expect(200);
     });
 });
@@ -65,6 +71,7 @@ describe('PUT em /editoras/id', () => {
         const spy = jest.spyOn(requisicao, 'request');
         await requisicao.request(app)
             .put(`/editoras/${idResposta}`)
+            .set('Authorization', `Bearer ${accessToken}`)
             .send(param)
             .expect(204);
 
@@ -76,6 +83,7 @@ describe('DELETE em /editoras/id', () => {
     it('Deletar o recurso adcionado', async () => {
         await request(app)
             .delete(`/editoras/${idResposta}`)
+            .set('Authorization', `Bearer ${accessToken}`)
             .expect(200);
     });
 });
