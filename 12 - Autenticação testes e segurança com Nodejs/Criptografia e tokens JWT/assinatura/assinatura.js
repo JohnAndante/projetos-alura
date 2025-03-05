d04c398ec9d81c989064bd5ceeffe0d8f5cc053f
JohnAndante <walker_contas@hotmail.com>
2025-03-04 22:28:35 -0300

    #12 - feat: End of class '04. Assinatura e JWT'

---
+++ 12 - Autenticação testes e segurança com Nodejs/Criptografia e tokens JWT/assinatura/assinatura.js
@@ -0,0 +1,25 @@
+import { generateKeyPairSync, createSign, createVerify } from 'crypto'
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa',
+    {
+        modulusLength: 2048,
+
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        },
+    }
+)
+
+let dados = "Essa string vai ser assinada!"
+const assinador = createSign('rsa-sha256');
+
+assinador.update(dados);
+
+const assinatura = assinador.sign(privateKey, 'hex');
+
+console.log(`Assinatura: ${assinatura}`)
